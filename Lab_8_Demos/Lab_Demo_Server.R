d5f29d4970d32deffb25bf2c4b88329dc4f56fd1
Superme-Abdiwahid <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-11-17 16:04:15 -0800

    Update Demo for 11/23

--- Lab_8_Demos/Lab_Demo_Server.R
+++ Lab_8_Demos/Lab_Demo_Server.R
@@ -6,27 +6,23 @@
 
 ## Load the dplyr library
 library(dplyr)
-
 ## Load the shiny library
 library(shiny)
-
 ## load the ggplot2 library
 library(ggplot2)
 
-
 ## load the maps library
 library(maps)
-
 ##load the mapproj library
 library(mapproj)
-
 ## source the Lab_Demo_ui.R
 source("Lab_Demo_ui.R")
 
+## load the plotly library
+library(plotly)
 ## Load the starbucks dataset Data/StarbucksData.csv" into a varible called `starbucks`
 
-starbucks <- read.csv("Data/StarbucksData.csv")
-
+starbucks <- read.csv("Data/StarbucksData.csv", stringsAsFactors = FALSE)
 
 ## The first set up you should do is set up a variable `Demo_server` and create a function
 ## by passing it an input and output. This would allow you to recieve input from users
@@ -38,7 +34,7 @@ starbucks <- read.csv("Data/StarbucksData.csv")
 ##1. Inside the server do the following create an output to render a ggplot. Name this
 ## starbucks_output
 
-## 2. Render a renderPlot for your plot. By calling renderPlot function()
+## 2. Render a renderPlotly for your plot. By calling renderPlot function()
 
 ## 3. Inside the renderPlot Change the columns in the starbucks data to be the following
 ## "Food", "Calories", "Fat (g)", "Carb (g)", "Fiber (g)", "Protein (g)"
@@ -58,17 +54,14 @@ starbucks <- read.csv("Data/StarbucksData.csv")
 
 ##Optionally set labs for ggtitle
 
+
 Demo_server <- function(input, output){
-  output$starbucks_output <- renderPlot({
+  output$starbucks_output <- renderPlotly({
     colnames(starbucks) <- c("Food", "Calories", "Fat (g)", "Carb (g)", "Fiber (g)", "Protein (g)")
-    
     starbucks_plot <- ggplot(data = starbucks)+
       geom_line(mapping = aes(x = `Calories`, y = starbucks[[input$nutrition_type]]), 
-                color = input$color_input)+
-      ggtitle("Starbucks Scatterplot")+
-      labs(x = "Calories", y = input$nutrition_type)
+                color = input$color_input)
     
     starbucks_plot
   })
 }
-
