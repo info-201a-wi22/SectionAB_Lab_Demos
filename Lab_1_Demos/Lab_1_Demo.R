ef2a0fe80c1b5e56dfb8e60f385de995092e0dbb
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-01-10 11:44:53 -0800

    Add solutions for lab 1

---
+++ Lab_1_Demos/Lab_1_Demo.R
@@ -0,0 +1,29 @@
+# Overview ----------------------------------------------------------------
+# For grading, it's important that you store your answers in the variable names
+# listed with each question in `backtics`.
+# For each prompt marked `Reflection`, please write a response
+# in your `README.md` file.
+
+## Create a variable `sum` which is the sum of 10 and 20
+
+sum <- 10 + 20
+
+
+## Create a variable `mood` which describes your mood
+
+mood <- "Happy"
+
+
+## Create a variable `date` which is Today's Date
+
+date <- "2022-01-10"
+
+##What is the max of these numbers 10, 40, 50, 100, 400, 135, 70 `max_total`
+
+max_total <- max(10, 40, 50, 100, 400, 135, 70)
+
+
+
+
+
+# Reflection: Create a header with your name.
