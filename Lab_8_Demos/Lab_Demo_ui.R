9a55175e21ecb5b7080638368bb8b71b2e8a22d8
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-02-28 21:27:16 -0800

    upload demo from 2/28 lab

--- Lab_8_Demos/Lab_Demo_ui.R
+++ Lab_8_Demos/Lab_Demo_ui.R
@@ -39,6 +39,7 @@ first_page <- tabPanel(
   h4("FirstPage"),
   p("My very first webpage I am coding"),
   p("I am publishing and writing my very first web page HORRAY!!!! YEYEYEYEYEY!!!"),
+  p("One plus one is equal = ", one_plus_one)
   img(src = "https://creative.starbucks.com/images/logos/logo-1987.png")
 )
 
